d8ebb25e0393277dbc535c3b70a21a8f606f6d92
Facundo Martin <facundomartin98@gmail.com>
2022-04-26 20:34:45 -0400

    added Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Sidebar.css";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
+import FlightIcon from "@mui/icons-material/Flight";
+import RocketLaunchIcon from "@mui/icons-material/RocketLaunch";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h2>Sidebar</h2>
       {/* NavIcons */}
+      <div className="sidebar__nav">
+        <MailOutlineIcon className="sidebar__nav-icon" />
+        <AccountBalanceWalletIcon className="sidebar__nav-icon" />
+        <FlightIcon className="sidebar__nav-icon rotate" />
+        <RocketLaunchIcon className="sidebar__nav-icon rotate" />
+        <FavoriteIcon className="sidebar__nav-icon" />
+      </div>
       {/* SettingsIcon */}
+      <div className="sidebar__settingsicon">
+        <SettingsIcon className="sidebar__nav-icon" />
+      </div>
     </div>
   );
 }
