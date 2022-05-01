38f23daf1d2c9489975abdb937cffe9481142701
Facundo Martin <facundomartin98@gmail.com>
2022-05-01 10:22:30 -0400

    addded toast

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
+import { useEffect } from "react";
 import "./App.css";
 import Content from "./components/Content";
 import Feed from "./components/Feed";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
+  const welcomeUser = (userName, toastHandler = toast) => {
+    toastHandler.success(`Welcome back ${userName}!`, {
+      style: {
+        background: "#06223c",
+        color: "#fff",
+      },
+    });
+  };
+
+  // This code should be executed when user succesfully connect to app using Metamask/other
+  useEffect(() => {
+    let userName = "@kenny";
+    welcomeUser(userName);
+  }, []);
+
   return (
     <div className="app">
+      <Toaster position="top-center" reverseOrder={false} />
       <Header />
 
       <div className="app__content">
